3c6f6c2b74d5aff7d00d4b0d9cbfa6ea48785df5
Jargerius <162357319+Jargerius@users.noreply.github.com>
2025-05-16 19:43:54 +0500

    Update AutoHeaterControl.ino

--- AutoHeaterControl/AutoHeaterControl.ino
+++ AutoHeaterControl/AutoHeaterControl.ino
@@ -7,8 +7,12 @@ Timer timer;
 #define heater_1 7  // Нагреватель №1
 #define heater_2 8  // Нагреватель №2
 int Temp;
+int flag = 1;
+unsigned long cur_ms = 0;
 unsigned long timer1 = 0;
 unsigned long timer2 = 0;
+bool relayOn = false;     // Состояние реле
+bool startTimer = false;  // Флаг запуска таймера
 bool quartzOn = true;
 bool stateHeater1 = false;
 bool stateHeater2 = false;
@@ -23,7 +27,7 @@ void setup() {
   digitalWrite(heater_1, LOW);
   digitalWrite(heater_2, LOW);
   Serial.begin(9600);
-  timer.every(20000, quartz);
+  timer.every(1800000, quartz);
   timer.every(1000, debug);
 }
 void debug(){
@@ -50,19 +54,21 @@ void loop() {
   timer.update();
   Temp = sensor.getTempInt();
 
-  if (millis() - timer1 >= 5000 && Temp <= 60 && stateHeater1 == false){ // Устанавливаем время через которое запустится первый нагреватель и температуру
+  if (millis() - timer1 >= 5000 && Temp <= 50 && stateHeater1 == false){ // Устанавливаем время через которое запустится первый нагреватель и температуру
     stateHeater1 = true;
+    timer1 = millis();
     digitalWrite(heater_1, HIGH);
     Serial.println("Heater 1 turn on");
-  } else if (millis() - timer2 >= 10000 && Temp <=40 && stateHeater2 == false) {
+  } else if (millis() - timer2 >= 10000 && Temp <=35 && stateHeater2 == false) {
     stateHeater2 = true;
+    timer2 = millis();
     digitalWrite(heater_2, HIGH); 
     Serial.println("Heater 2 turn on");
-  } else if (Temp >= 50 && stateHeater2 == true){
+  } else if (Temp >= 45 && stateHeater2 == true){
     stateHeater2 = false;
     digitalWrite(heater_2, LOW);
     Serial.println("Heater 2 turn off");
-  } else if (Temp >= 60){
+  } else if (Temp >= 55){
     stateHeater1 = false;
     stateHeater2 = false;
     digitalWrite(heater_1, LOW);
